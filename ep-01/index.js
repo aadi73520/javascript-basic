1c7943305a892f33a4a3c68e7fb132e98e0bb9fe
aadi73520 <aadityakrchy01@gmail.com>
2024-08-23 23:11:20 +0530

    ep-01

---
+++ ep-01/index.js
@@ -0,0 +1,17 @@
+//Temlate Literals:
+
+
+const age=20;
+const fullName="john Doe";
+//output:Hello Mr john Doe your age is:20
+console.log("Hello Mr "+fullName+" your age is: "+age);
+console.log(`Hello Mr ${fullName} your age is: ${age}`);//using Template litereals
+
+
+//Q.print sum of two numbers
+const a=10;
+const b=20;
+console.log("20"+a);
+console.log("Sum of "+a+" and "+b+" is "+a+b);
+console.log(`Sum of ${a} and ${b} is ${a+b}`);//in string literals you can perform mathemetical operation but in case of string you cannot do math.random()
+
