e7433b1b23242f2db383c4cdb1873d89da203de3
aadi73520 <aadityakrchy01@gmail.com>
2024-08-24 00:50:06 +0530

    ep-04

---
+++ ep-04/index.js
@@ -0,0 +1,14 @@
+//Let and const declarations
+//let and const kab use karte hn for making a variable in javascript
+
+const a=10;//we cannot change const keyword
+// a=20;
+// console.log(a);//it gives error
+
+let b=100;//we can further modified the value of let type variable
+b=200;
+console.log(b);
+
+var x=20;
+x=40;
+console.log(x);
