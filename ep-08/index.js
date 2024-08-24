11e6144317d832b8b2192d45cad46450d5c45820
aadi73520 <aadityakrchy01@gmail.com>
2024-08-24 16:52:30 +0530

    ep-08

---
+++ ep-08/index.js
@@ -0,0 +1,10 @@
+//Error object (basically error ko display karna)
+
+// const x=200;
+// x=100;
+// console.log(x);
+
+const myError=new Error("This is my error");
+console.log(myError);
+
+throw new Error("Something went wrong..");
