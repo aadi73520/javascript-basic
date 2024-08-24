7fd7f598852a1ce210568f0b1c3722800aa20016
aadi73520 <aadityakrchy01@gmail.com>
2024-08-24 17:15:14 +0530

    ep-09

---
+++ ep-09/index.js
@@ -0,0 +1,11 @@
+//Brower Storage
+
+//diffrence between local storage vs session storage prefer on MDN website
+localStorage.setItem("token","afjakfhbad");//key value pair
+console.log(localStorage.getItem("token"));//for printing
+
+localStorage.clear();//clear data in local storage(you can check localStorage function just click localStorage.)
+
+sessionStorage.setItem("token","ahfduaiufa");
+
+
